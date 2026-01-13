4dc19c442711c19427d7d3c5933c8e672a25e4b2
Chi Wing Lam <clam@bp-3.com>
2026-01-13 13:39:37 +0100

    Added rule and test for no-job-worker-user-task-implementation-type.

---
+++ test/no-job-worker-user-task-implementation-type.spec.js
@@ -0,0 +1,66 @@
+/*================================================================================
+ =
+ = Licensed Materials - Property of BP3 Global
+ =
+ =  bpmn-rules
+ =
+ = Copyright © BP3 Global. 2026. All Rights Reserved.
+ = This software is subject to copyright protection under
+ = the laws of the United States, United Kingdom and other countries.
+ =
+ =================================================================================*/
+
+const { createModdle } = require('bpmnlint/lib/testers/helper');
+const { verifyRule } = require('./ruleTestRunner');
+
+verifyRule({
+  valid: [
+    {
+      name: 'UserTask with a valid external form',
+      moddleElement: createModdle(
+        `
+<bpmn:userTask xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:zeebe="http://camunda.org/schema/zeebe/1.0" xmlns:modeler="http://camunda.org/schema/modeler/1.0" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Activity_1mkqbux" name="external form">
+  <bpmn:extensionElements>
+    <zeebe:userTask />
+    <zeebe:formDefinition externalReference="" />
+  </bpmn:extensionElements>
+</bpmn:userTask>
+        `,
+        'bpmn:UserTask'
+      ),
+    },
+    {
+      name: 'UserTask with a Camunda form',
+      moddleElement: createModdle(
+        `
+<bpmn:userTask xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:zeebe="http://camunda.org/schema/zeebe/1.0" xmlns:modeler="http://camunda.org/schema/modeler/1.0" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Activity_0b74psj" name="Do Something">
+  <bpmn:extensionElements>
+    <zeebe:userTask />
+    <zeebe:assignmentDefinition assignee="SampleUser" />
+  </bpmn:extensionElements>
+</bpmn:userTask>
+        `,
+        'bpmn:UserTask'
+      ),
+    },
+  ],
+  invalid: [
+    {
+      name: 'UserTask with a job worker external form',
+      moddleElement: createModdle(
+        `
+<bpmn:userTask xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:zeebe="http://camunda.org/schema/zeebe/1.0" xmlns:modeler="http://camunda.org/schema/modeler/1.0" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Activity_JobWorkerUserTask" name="external form">
+  <bpmn:extensionElements>
+    <zeebe:formDefinition externalReference="" />
+  </bpmn:extensionElements>
+</bpmn:userTask>
+        `,
+        'bpmn:UserTask'
+      ),
+      report: {
+        id: 'Activity_JobWorkerUserTask',
+        message: 'User Task has disallowed Implementation Type: Job Worker',
+      },
+    },
+  ],
+});
