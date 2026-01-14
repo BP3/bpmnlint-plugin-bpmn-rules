05dd8302de77228533101829d01c2336d7e5832d
Chi Wing Lam <clam@bp-3.com>
2026-01-14 13:54:32 +0100

    Add helper function for XML fragment generation

--- test/helper.js
+++ test/helper.js
@@ -30,6 +30,15 @@ function _verifyRule(ruleName, testCases) {
   }
 }
 
+function generateFragment(xmlString) {
+  return `
+    <bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:zeebe="http://camunda.org/schema/zeebe/1.0" xmlns:modeler="http://camunda.org/schema/modeler/1.0" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Definitions_0mryikp">
+      ${xmlString}
+    </bpmn:definitions>
+    `;
+}
+
 module.exports = {
   verifyRule,
+  generateFragment,
 };
