b1b91ea72f833c883c539db7c9204c3dda45b172
Chi Wing Lam <12345192+cwlam@users.noreply.github.com>
2026-01-19 14:44:32 +0100

    Implement rules and configs initialization in index.js.

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-module.exports = {};
+// /*================================================================================
+//  =
+//  = Licensed Materials - Property of BP3 Global
+//  =
+//  =  bpmnlint-plugin-bpmn-rules
+//  =
+//  = Copyright © BP3 Global Inc. 2026. All Rights Reserved.
+//  = This software is subject to copyright protection under
+//  = the laws of the United States, United Kingdom and other countries.
+//  =
+//  =================================================================================*/
+
+const { logger } = require('./logger');
+const fs = require('fs');
+const path = require('path');
+
+// Get the prefix for this plugin from the package.json if it's set
+const { name } = require('./package.json');
+//NOTE: not using the start of string ^ wildcard because it's not the case when there's a @publisherName present
+const prefix = name.replace(/bpmnlint-plugin-/, '');
+
+const rulesPath = './rules';
+
+//helper to prepare the rules path for the plugin
+function getRulePath(basePath, ruleName) {
+  return `${basePath != null && basePath.trim().length > 0 ? `${basePath}/` : ``}${ruleName}`;
+}
+
+let bpmnlintRulesConfig = {
+  configs: {
+    all: { rules: {} },
+    'getting-started': { rules: {} },
+    advanced: { rules: {} },
+  },
+  rules: {},
+};
+
+function addRule(ruleName, severity) {
+
+  const ruleFile = getRulePath(rulesPath, ruleName);
+  if (!fs.existsSync(path.resolve(ruleFile + '.js'))) {
+    logger.warn(`Warning: Rule file does not exist: ${ruleFile}` + '.js');
+    return; // skip adding
+  }
+
+  bpmnlintRulesConfig.rules[ruleName] = ruleFile;
+
+  var rulesets = Object.keys(severity);
+  rulesets.forEach((item) => {
+    const prefixedRuleName = `${prefix != null && prefix.trim().length > 0 ? `${prefix}/` : ``}${ruleName}`;
+    bpmnlintRulesConfig.configs[item].rules[prefixedRuleName] = severity[item];
+  });
+}
+
+addRule('activity-with-default-id', { "all": "warn", "getting-started": "warn", "advanced": "error" });
+addRule('activity-without-type', { "all": "info", "getting-started": "info", "advanced": "info" });
+addRule('artifact-with-default-id', { "all": "off", "getting-started": "off", "advanced": "off" });
+addRule('event-with-default-id', { "all": "info", "getting-started": "info", "advanced": "warn" });
+addRule('error-with-default-name', { "all": "warn", "getting-started": "warn", "advanced": "error" });
+addRule('escalation-with-default-name', { "all": "info", "getting-started": "info", "advanced": "warn" });
+addRule('exclusive-gateway-has-default-flow', { "all": "info", "getting-started": "info", "advanced": "warn" });
+addRule('gateway-with-default-id', { "all": "info", "getting-started": "info", "advanced": "warn" });
+addRule('lane-with-default-id', { "all": "info", "getting-started": "off", "advanced": "info" });
+addRule('message-flow-with-default-id', { "all": "info", "getting-started": "info", "advanced": "info" });
+addRule('message-with-default-name', { "all": "warn", "getting-started": "warn", "advanced": "error" });
+addRule('no-job-worker-user-task-implementation-type', { "all": "error", "getting-started": "error", "advanced": "error" });
+addRule('participant-with-default-id', { "all": "info", "getting-started": "off", "advanced": "info" });
+addRule('process-with-default-id', { "all": "error", "getting-started": "error", "advanced": "error" });
+addRule('process-with-default-name', { "all": "warn", "getting-started": "info", "advanced": "warn" });
+addRule('sequence-flow-with-default-id', { "all": "info", "getting-started": "off", "advanced": "info" });
+addRule('signal-with-default-name', { "all": "warn", "getting-started": "info", "advanced": "warn" });
+addRule('subprocess-with-default-id', { "all": "info", "getting-started": "info", "advanced": "warn" });
+addRule('user-task-without-assignment-details', { "all": "warn", "getting-started": "warn", "advanced": "error" });
+addRule('variable-name-with-invalid-character', { "all": "error", "getting-started": "warn", "advanced": "error" });
+
+module.exports = bpmnlintRulesConfig;
