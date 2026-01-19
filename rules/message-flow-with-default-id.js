6756ad05b8a4111375877a4fb6016a7aa647296f
Chi Wing Lam <12345192+cwlam@users.noreply.github.com>
2026-01-19 16:48:21 +0100

    Add message-flow-with-default-id rules and tests.

---
+++ rules/message-flow-with-default-id.js
@@ -0,0 +1,35 @@
+/*================================================================================
+ =
+ = Licensed Materials - Property of BP3 Global
+ =
+ =  bpmnlint-plugin-bpmn-rules
+ =
+ = Copyright © BP3 Global Inc. 2026. All Rights Reserved.
+ = This software is subject to copyright protection under
+ = the laws of the United States, United Kingdom and other countries.
+ =
+ =================================================================================*/
+
+const { isAny } = require('bpmnlint-utils');
+
+/**
+ * Rule that reports whether an artifact does not have a significant id (applies to: Flows/MessageFlow)
+ */
+module.exports = function () {
+  function check(node, reporter) {
+    if (isAny(node, ['bpmn:MessageFlow'])) {
+      let isNotSignificantId = false;
+      let notSignificantNamePattern = /^Flow_\d\w{6}$/i;
+      isNotSignificantId = notSignificantNamePattern.test(node.id);
+
+      //output
+      if (isNotSignificantId) {
+        reporter.report(node.id, 'Message flow has a default id. Please provide a significant id!');
+      }
+    }
+  }
+
+  return {
+    check: check,
+  };
+};
---
+++ test/message-flow-with-default-id.spec.js
@@ -0,0 +1,47 @@
+/*================================================================================
+ =
+ = Licensed Materials - Property of BP3 Global
+ =
+ =  bpmnlint-plugin-bpmn-rules
+ =
+ = Copyright © BP3 Global Inc. 2026. All Rights Reserved.
+ = This software is subject to copyright protection under
+ = the laws of the United States, United Kingdom and other countries.
+ =
+ =================================================================================*/
+
+const { createModdle } = require('bpmnlint/lib/testers/helper');
+const { verifyRule, generateFragment } = require('./helper');
+
+verifyRule(__filename, {
+  valid: [
+    {
+      name: 'Process with valid message flow id',
+      moddleElement: createModdle(
+        generateFragment(`
+  <bpmn:collaboration id="Collaboration_1u0tsgp">
+    <bpmn:messageFlow id="MessageFlow_Task1_To_Task2" sourceRef="StartProcessActivity" targetRef="Activity_150fsc7" />
+  </bpmn:collaboration>
+        `)
+      ),
+    },
+  ],
+  invalid: [
+    {
+      name: 'Process with default message flow id',
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:collaboration id="Collaboration_1u0tsgp">
+  <bpmn:messageFlow id="Flow_0qwei4j" sourceRef="StartProcessActivity" targetRef="Activity_150fsc7" />
+</bpmn:collaboration>
+        `)
+      ),
+      report: [
+        {
+          id: 'Flow_0qwei4j',
+          message: 'Message flow has a default id. Please provide a significant id!',
+        },
+      ],
+    },
+  ],
+});
