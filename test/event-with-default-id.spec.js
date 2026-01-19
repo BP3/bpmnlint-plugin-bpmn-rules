b8464f965c2575e3f915f0d1d4e8a9c138a699bf
Chi Wing Lam <12345192+cwlam@users.noreply.github.com>
2026-01-19 16:11:19 +0100

    Add event-with-default-id rule and tests.

---
+++ test/event-with-default-id.spec.js
@@ -0,0 +1,49 @@
+/*================================================================================
+ =
+ = Licensed Materials - Property of BP3 Global
+ =
+ =  bpmnlint-plugin-bpmn-rules
+ =
+ = Copyright © BP3 Global Inc. 2026. All Rights Reserved.
+ = This software is subject to copyright protection under
+ = the laws of the United States, United Kingdom and other countries.
+ =
+ =================================================================================*/
+
+const { createModdle } = require('bpmnlint/lib/testers/helper');
+const { verifyRule, generateFragment } = require('./helper');
+
+verifyRule(__filename, {
+  valid: [
+    {
+      name: 'Process with valid event id',
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:process id="Process_078ti4z">
+  <bpmn:boundaryEvent id="BoundaryEvent" attachedToRef="Activity">
+  </bpmn:boundaryEvent>
+</bpmn:process>
+        `)
+      ),
+    },
+  ],
+  invalid: [
+    {
+      name: 'Process with default event id',
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:process id="Process_078ti4z">
+  <bpmn:boundaryEvent id="Event_1490aig" attachedToRef="Activity">
+  </bpmn:boundaryEvent>
+</bpmn:process>
+        `)
+      ),
+      report: [
+        {
+          id: 'Event_1490aig',
+          message: 'Event has a default id. Please provide a significant id!',
+        },
+      ],
+    },
+  ],
+});
