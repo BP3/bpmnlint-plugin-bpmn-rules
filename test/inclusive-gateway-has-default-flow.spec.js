3a3c9fea9c23042ba7c0f9c5bbde59cab7e52ed7
Maryna Kalaur <mkalaur@bp-3.com>
2026-01-19 15:40:25 +0000

    - create new rule inclusive-gateway-has-default-flow - tests added

--- test/inclusive-gateway-has-default-flow.spec.js
+++ test/inclusive-gateway-has-default-flow.spec.js
@@ -18,19 +18,13 @@ verifyRule(__filename, {
     {
       name: "Process gateway with default flow",
       moddleElement: createModdle(
-        generateFragment('<bpmn:inclusiveGateway id="Gateway_0knccn5" default="Flow_1dh1e1n">' +
-                         '<bpmn:outgoing>Flow_1dh1e1n</bpmn:outgoing>' +
-                         '<bpmn:outgoing>Flow_1sijn6i</bpmn:outgoing>' +
-                         '</bpmn:inclusiveGateway>' +
-                         '<bpmn:sequenceFlow id="Flow_1dh1e1n" sourceRef="Gateway_0knccn5" targetRef="A" />' +
-                         '<bpmn:sequenceFlow id="Flow_1sijn6i" sourceRef="Gateway_0knccn5" targetRef="B" />'
-       )
+        generateFragment('<bpmn:inclusiveGateway id="Gateway_0knccn5" default="Flow_1dh1e1n"> <bpmn:outgoing>Flow_1dh1e1n</bpmn:outgoing> <bpmn:outgoing>Flow_1sijn6i</bpmn:outgoing> </bpmn:inclusiveGateway> <bpmn:sequenceFlow id="Flow_1dh1e1n" sourceRef="Gateway_0knccn5" targetRef="A" /> <bpmn:sequenceFlow id="Flow_1sijn6i" sourceRef="Gateway_0knccn5" targetRef="B" />' )
       )
     },
     {
       name: "Process with a gateway that has a default and a join without default",
       moddleElement: createModdle(
-         generateFragment('<bpmn:inclusiveGateway id="Gateway_1yay0j2"><bpmn:outgoing>Flow_14weok5</bpmn:outgoing></bpmn:inclusiveGateway>')
+        generateFragment('<bpmn:inclusiveGateway id="Gateway_1yay0j2"><bpmn:outgoing>Flow_14weok5</bpmn:outgoing></bpmn:inclusiveGateway>')
       )
     }
   ],
@@ -38,22 +32,14 @@ verifyRule(__filename, {
     {
       name: "Process gateway without default flow",
       moddleElement: createModdle(
-        generateFragment('<bpmn:process id="Process_1" isExecutable="false">' +
-                         '<bpmn:inclusiveGateway id="Gateway_0knccn5">' +
-                         '<bpmn:outgoing>Flow_1</bpmn:outgoing>' +
-                         '<bpmn:outgoing>Flow_2</bpmn:outgoing>' +
-                         '</bpmn:inclusiveGateway>' +
-                         '<bpmn:sequenceFlow id="Flow_1" sourceRef="Gateway_0knccn5" targetRef="Task_A" />' +
-                         '<bpmn:sequenceFlow id="Flow_2" sourceRef="Gateway_0knccn5" targetRef="Task_B" />' +
-                         '<bpmn:task id="Task_A" />' +
-                         '<bpmn:task id="Task_B" />' +
-                         '</bpmn:process>'
-        )
+        generateFragment('<bpmn:process id="Process_1" isExecutable="false"> <bpmn:inclusiveGateway id="Gateway_0knccn5"> <bpmn:outgoing>Flow_1</bpmn:outgoing> <bpmn:outgoing>Flow_2</bpmn:outgoing> </bpmn:inclusiveGateway> <bpmn:sequenceFlow id="Flow_1" sourceRef="Gateway_0knccn5" targetRef="Task_A" /> <bpmn:sequenceFlow id="Flow_2" sourceRef="Gateway_0knccn5" targetRef="Task_B" /><bpmn:task id="Task_A" /> <bpmn:task id="Task_B" /> </bpmn:process>' )
       ),
-      report: {
-        id: "Gateway_0knccn5",
-        message: "Inclusive Gateway should always have a default sequence flow."
-      }
+      report: [
+        {
+          id: "Gateway_0knccn5",
+          message: "Inclusive Gateway should always have a default sequence flow."
+        }
+      ]
     }
   ]
 });
