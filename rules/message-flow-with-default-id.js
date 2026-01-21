6e3f9bfff77246b9ff031fc99325994552091e2c
Chi Wing Lam <12345192+cwlam@users.noreply.github.com>
2026-01-21 09:48:06 +0100

    Rename variable for default id check.

--- rules/message-flow-with-default-id.js
+++ rules/message-flow-with-default-id.js
@@ -19,8 +19,8 @@ module.exports = function () {
   function check(node, reporter) {
     if (isAny(node, ['bpmn:MessageFlow'])) {
       let isNotSignificantId = false;
-      let notSignificantNamePattern = /^Flow_\d\w{6}$/i;
-      isNotSignificantId = notSignificantNamePattern.test(node.id);
+      let notSignificantIdPattern = /^Flow_\d\w{6}$/i;
+      isNotSignificantId = notSignificantIdPattern.test(node.id);
 
       //output
       if (isNotSignificantId) {
