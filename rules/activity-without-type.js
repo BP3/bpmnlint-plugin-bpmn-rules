1e6f506f83a0799292fe78e626ad6b7b4c5376fd
Maryna Kalaur <mkalaur@bp-3.com>
2026-01-22 22:45:08 +0000

    - migrate activity-without-type rule -tests added

---
+++ rules/activity-without-type.js
@@ -0,0 +1,33 @@
+/*================================================================================
+ =
+ = Licensed Materials - Property of BP3 Global
+ =
+ =  bpmnlint-plugin-bpmn-rules
+ =
+ = Copyright © BP3 Global Inc. 2026. All Rights Reserved.
+ = This software is subject to copyright protection under
+ = the laws of the United States, United Kingdom and other countries.
+ =
+ =================================================================================*/
+
+const { is } = require('bpmnlint-utils');
+
+/**
+ * Rule that reports whether an activity does not have a type (applies to: Tasks/Activities)
+ */
+module.exports = function () {
+  function check(node, reporter) {
+    if (is(node, 'bpmn:Activity')) {
+      let isTaskTypeSet = node["$type"] != 'bpmn:Task';
+
+      //output
+      if (!isTaskTypeSet) {
+        reporter.report(node.id, 'Artifact has no type. Please provide a task type!');
+      }
+    }
+  }
+
+  return {
+    check: check
+  };
+};
---
+++ test/activity-without-type.spec.js
@@ -0,0 +1,45 @@
+/*================================================================================
+ =
+ = Licensed Materials - Property of BP3 Global
+ =
+ =  bpmnlint-plugin-bpmn-rules
+ =
+ = Copyright © BP3 Global Inc. 2026. All Rights Reserved.
+ = This software is subject to copyright protection under
+ = the laws of the United States, United Kingdom and other countries.
+ =
+ =================================================================================*/
+
+const { createModdle } = require('bpmnlint/lib/testers/helper');
+const { verifyRule, generateFragment } = require('./helper');
+
+verifyRule(__filename, {
+  valid: [
+    {
+      name: "Process activity with type",
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:serviceTask id="ServiceTask_Valid" name="Charge Credit Card" />
+        `)
+      ),
+    },
+  ],
+  invalid: [
+    {
+      name: "Process activity without any type",
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:process id="Process_178ti4z" isExecutable="false">
+  <bpmn:task id="Activity_0mw9waf" name="Proceed Payment" />
+</bpmn:process>
+        `)
+      ),
+      report: [
+        {
+          id: "Activity_0mw9waf",
+          message: "Artifact has no type. Please provide a task type!"
+        },
+      ],
+    },
+  ],
+});
