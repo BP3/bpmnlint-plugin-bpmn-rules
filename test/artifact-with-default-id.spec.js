d7bd8f7df3bc29948434401a3c3821b95f1ff0b7
Chi Wing Lam <12345192+cwlam@users.noreply.github.com>
2026-01-26 15:10:03 +0100

    add artifact-with-default-id rule and tests.

---
+++ test/artifact-with-default-id.spec.js
@@ -0,0 +1,64 @@
+/*================================================================================
+ =
+ = Licensed Materials - Property of BP3 Global
+ =
+ =  bpmnlint-plugin-bpmn-rules
+ =
+ = Copyright © BP3 Global Inc. 2026. All Rights Reserved.
+ = This software is subject to copyright protection under
+ = the laws of the United States, United Kingdom and other countries.
+ =
+ =================================================================================*/
+
+const { createModdle } = require('bpmnlint/lib/testers/helper');
+const { verifyRule, generateFragment } = require('./helper');
+
+verifyRule(__filename, {
+  valid: [
+    {
+      name: 'Process with valid activity and gateway ids',
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:process id="Process_1s1qrpb">
+</bpmn:process>
+        `)
+      ),
+    },
+  ],
+  invalid: [
+    {
+      name: 'Process with default activity id',
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:process id="Process_1s1qrpb">
+  <bpmn:task id="Activity_1w64wtr" name="Review Request">
+  </bpmn:task>
+</bpmn:process>
+        `)
+      ),
+      report: [
+        {
+          id: 'Activity_1w64wtr',
+          message: 'Artifact has a default id. Please provide a significant id!',
+        },
+      ],
+    },
+    {
+      name: 'Process with default gateway id',
+      moddleElement: createModdle(
+        generateFragment(`
+<bpmn:process id="Process_1s1qrpb">
+  <bpmn:exclusiveGateway id="Gateway_0hdljrh">
+  </bpmn:exclusiveGateway>
+</bpmn:process>
+        `)
+      ),
+      report: [
+        {
+          id: 'Gateway_0hdljrh',
+          message: 'Artifact has a default id. Please provide a significant id!',
+        },
+      ],
+    },
+  ],
+});
