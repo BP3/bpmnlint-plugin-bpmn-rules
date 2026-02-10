1ad464e11b5bbace7764e833b6eea3fe36f5b578
mkalaur <mkalaur@bp-3.com>
2026-02-10 09:51:41 +0000

    Add Camunda 8.7 and Camunda 8.8 versions ruleset (#78)
    * - add Camunda 8.7 and Camunda 8.8 versions ruleset
    * - fix eslint
    * - add version-specific rules for Camunda 8.6, 8.7, and 8.8
    - Implements rule inheritance
    - Implements tuple format for all versioned configs.
    * - escalate 'user-task-without-assignment-details' to 'error' in camunda-8-8
    * - update the severity levels
    * - set no-job-worker-user-task-implementation-type to warn in camunda-8-6 to reflect deprecation
    - simplified addRule
    * - implement versioned rulesets for Camunda 8.5-8.8
    * - refactor the conversion loop logic, remove rule version
    * - introduce 'recommended-base' and 'strict-base'

--- index.js
+++ index.js
@@ -30,9 +30,13 @@ let bpmnlintRulesConfig = {
     all: { rules: {} },
     recommended: { rules: {} },
     strict: { rules: {} },
+    'recommended-base': { rules: {} },
+    'strict-base': { rules: {} },
   },
   rules: {},
 };
+
+let ruleArrays = {};
 
 function addRule(ruleName, severity) {
   const ruleFile = getRulePath(rulesPath, ruleName);
@@ -43,38 +47,92 @@ function addRule(ruleName, severity) {
 
   bpmnlintRulesConfig.rules[ruleName] = ruleFile;
 
-  const allSeverity = severity.all ?? 'info';
-  const rulesetSeverities = { all: allSeverity, ...severity };
+  const rulesetSeverities = { all: severity.all ?? 'info', ...severity };
 
   Object.keys(rulesetSeverities).forEach((ruleset) => {
-    // skip if severity is off
-    if (rulesetSeverities[ruleset] === 'off') return;
-
-    const prefixedRuleName = prefix ? `${prefix}/${ruleName}` : ruleName;
-    bpmnlintRulesConfig.configs[ruleset].rules[prefixedRuleName] = rulesetSeverities[ruleset];
+    ruleArrays[ruleset] = ruleArrays[ruleset] || [];
+    ruleArrays[ruleset].push({ name: ruleName, severity: rulesetSeverities[ruleset] });
   });
 }
 
-addRule('activity-with-default-id', { recommended: 'warn', strict: 'error' });
-addRule('activity-without-type', { recommended: 'info', strict: 'info' });
-addRule('artifact-with-default-id', { recommended: 'off', strict: 'off' });
-addRule('event-with-default-id', { recommended: 'info', strict: 'warn' });
-addRule('error-with-default-name', { recommended: 'warn', strict: 'error' });
-addRule('escalation-with-default-name', { recommended: 'info', strict: 'warn' });
-addRule('exclusive-gateway-has-default-flow', { recommended: 'info', strict: 'warn' });
-addRule('inclusive-gateway-has-default-flow', { recommended: 'info', strict: 'warn' });
-addRule('gateway-with-default-id', { recommended: 'info', strict: 'warn' });
-addRule('lane-with-default-id', { recommended: 'off', strict: 'info' });
-addRule('message-flow-with-default-id', { recommended: 'info', strict: 'info' });
-addRule('message-with-default-name', { recommended: 'warn', strict: 'error' });
-addRule('no-job-worker-user-task-implementation-type', { recommended: 'error', strict: 'error' });
-addRule('participant-with-default-id', { recommended: 'off', strict: 'info' });
-addRule('process-with-default-id', { recommended: 'error', strict: 'error' });
-addRule('process-with-empty-name', { recommended: 'info', strict: 'error' });
-addRule('sequence-flow-with-default-id', { recommended: 'off', strict: 'info' });
-addRule('signal-with-default-name', { recommended: 'info', strict: 'warn' });
-addRule('subprocess-with-default-id', { recommended: 'info', strict: 'warn' });
-addRule('user-task-without-assignment-details', { recommended: 'warn', strict: 'error' });
-addRule('variable-name-with-invalid-character', { recommended: 'warn', strict: 'error' });
+// baseline severities
+addRule('activity-with-default-id', { 'recommended-base': 'warn', 'strict-base': 'error' });
+addRule('activity-without-type', { 'recommended-base': 'info', 'strict-base': 'info' });
+addRule('artifact-with-default-id', { 'recommended-base': 'off', 'strict-base': 'off' });
+addRule('event-with-default-id', { 'recommended-base': 'info', 'strict-base': 'warn' });
+addRule('error-with-default-name', { 'recommended-base': 'warn', 'strict-base': 'error' });
+addRule('escalation-with-default-name', { 'recommended-base': 'info', 'strict-base': 'warn' });
+addRule('exclusive-gateway-has-default-flow', { 'recommended-base': 'info', 'strict-base': 'warn' });
+addRule('inclusive-gateway-has-default-flow', { 'recommended-base': 'info', 'strict-base': 'warn' });
+addRule('gateway-with-default-id', { 'recommended-base': 'info', 'strict-base': 'warn' });
+addRule('lane-with-default-id', { 'recommended-base': 'off', 'strict-base': 'info' });
+addRule('message-flow-with-default-id', { 'recommended-base': 'info', 'strict-base': 'info' });
+addRule('message-with-default-name', { 'recommended-base': 'warn', 'strict-base': 'error' });
+addRule('no-job-worker-user-task-implementation-type', { 'recommended-base': 'off', 'strict-base': 'off' });
+addRule('participant-with-default-id', { 'recommended-base': 'off', 'strict-base': 'info' });
+addRule('process-with-default-id', { 'recommended-base': 'error', 'strict-base': 'error' });
+addRule('process-with-empty-name', { 'recommended-base': 'info', 'strict-base': 'error' });
+addRule('sequence-flow-with-default-id', { 'recommended-base': 'off', 'strict-base': 'info' });
+addRule('signal-with-default-name', { 'recommended-base': 'info', 'strict-base': 'warn' });
+addRule('subprocess-with-default-id', { 'recommended-base': 'info', 'strict-base': 'warn' });
+addRule('user-task-without-assignment-details', { 'recommended-base': 'warn', 'strict-base': 'error' });
+addRule('variable-name-with-invalid-character', { 'recommended-base': 'warn', 'strict-base': 'error' });
+
+// Version Specific RECOMMENDED
+ruleArrays['camunda-8-5-recommended'] = [
+  ...ruleArrays['recommended-base'].filter((rule) => rule.name !== 'no-job-worker-user-task-implementation-type'),
+  { name: 'no-job-worker-user-task-implementation-type', severity: 'info' },
+];
+
+ruleArrays['camunda-8-6-recommended'] = [
+  ...ruleArrays['camunda-8-5-recommended'].filter((rule) => rule.name !== 'no-job-worker-user-task-implementation-type'),
+  { name: 'no-job-worker-user-task-implementation-type', severity: 'warn' },
+];
+
+ruleArrays['camunda-8-7-recommended'] = [
+  ...ruleArrays['camunda-8-6-recommended'].filter((rule) => rule.name !== 'no-job-worker-user-task-implementation-type'),
+  { name: 'no-job-worker-user-task-implementation-type', severity: 'error' },
+];
+
+ruleArrays['camunda-8-8-recommended'] = [
+  ...ruleArrays['camunda-8-7-recommended'].filter((rule) => rule.name !== 'user-task-without-assignment-details'),
+  { name: 'user-task-without-assignment-details', severity: 'off' },
+];
+
+// 3. Version Specific STRICT
+ruleArrays['camunda-8-5-strict'] = [
+  ...ruleArrays['strict-base'].filter((rule) => rule.name !== 'no-job-worker-user-task-implementation-type'),
+  { name: 'no-job-worker-user-task-implementation-type', severity: 'info' },
+];
+
+ruleArrays['camunda-8-6-strict'] = [
+  ...ruleArrays['camunda-8-5-strict'].filter((rule) => rule.name !== 'no-job-worker-user-task-implementation-type'),
+  { name: 'no-job-worker-user-task-implementation-type', severity: 'warn' },
+];
+
+ruleArrays['camunda-8-7-strict'] = [
+  ...ruleArrays['camunda-8-6-strict'].filter((rule) => rule.name !== 'no-job-worker-user-task-implementation-type'),
+  { name: 'no-job-worker-user-task-implementation-type', severity: 'error' },
+];
+
+ruleArrays['camunda-8-8-strict'] = [
+  ...ruleArrays['camunda-8-7-strict'].filter((rule) => rule.name !== 'user-task-without-assignment-details'),
+  { name: 'user-task-without-assignment-details', severity: 'off' },
+];
+
+// Update to the latest version
+ruleArrays['recommended'] = ruleArrays['camunda-8-8-recommended'];
+ruleArrays['strict'] = ruleArrays['camunda-8-8-strict'];
+
+// Conversion Loop
+Object.keys(ruleArrays).forEach((ruleSet) => {
+  bpmnlintRulesConfig.configs[ruleSet] = bpmnlintRulesConfig.configs[ruleSet] || { rules: {} };
+
+  for (var idx = 0; idx < ruleArrays[ruleSet].length; ++idx) {
+    const rule = ruleArrays[ruleSet][idx];
+    const prefixedRuleName = prefix ? `${prefix}/${rule.name}` : rule.name;
+    bpmnlintRulesConfig.configs[ruleSet].rules[prefixedRuleName] = rule.severity;
+  }
+});
 
 module.exports = bpmnlintRulesConfig;
